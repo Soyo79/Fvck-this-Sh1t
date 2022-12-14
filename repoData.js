6bad4a4b5e7ce97f8cb879c64bf38911652243ad
Soyo79 <96707994+Soyo79@users.noreply.github.com>
2022-12-14 16:47:56 -0600

    Update repoData.js

--- repoData.js
+++ repoData.js
@@ -1,12 +1,12 @@
 //Add Name of your Repo here
-exports.name = 'Example Repo';
+exports.name = 'Death';
 //Add Name and Type of Image Icon you want to use here (Must be PNG)
-exports.icon = 'icon.png';
+exports.icon = 'Sex.png';
 //Add the folder name where all your emotes are saved
-exports.pathName = 'emotes';
+exports.pathName = 'Emoji';
 //Add your name here, or remove the line if you don't need your name displayed under the repo's title in our clients
-exports.author = 'Joe';
+exports.author = 'Sex';
 //Add a Description here, useful for SEO and Web Crawlers, or sharing to social websites. Remove the line if you don't care about Sharing.
-exports.description = 'Example Description';
+exports.description = 'Fuck You!';
 //Add Keywords here, useful for SEO and Web Crawlers. Remove the line if you don't care about SEO/Web Crawlers.
-exports.keywords = 'Nitroless, nitroless, iOS, macOS, Android, Windows, Emotes, Discord, discord, Nitro, nitro';
+exports.keywords = 'Nitroless, nitroless, iOS, macOS, Android, Windows, Emotes, Discord, discord, Nitro, nitro, 1';
